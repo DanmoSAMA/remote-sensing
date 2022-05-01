c6e9a0ebc04f855f343e68c01a8f968db90ad294
DanmoSAMA <772523546@qq.com>
2022-05-01 10:51:05 +0800

    完成home页中间图片

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
-// import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Create from './components/Create'
+import Picture from './components/Picture'
 import { homeStyles } from './styles'
 
 function Home() {
   return (
     <Grid sx={homeStyles.body}>
       <Create />
+      <Picture />
     </Grid>
   )
 }
