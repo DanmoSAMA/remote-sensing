7df4f64cbf2febd34c35ff37c1268a1f7a4b9e53
DanmoSAMA <772523546@qq.com>
2022-05-12 09:37:54 +0800

    修复一点bug

--- src/pages/ChangeDetection/components/Main/components/Image/index.tsx
+++ src/pages/ChangeDetection/components/Main/components/Image/index.tsx
@@ -5,6 +5,19 @@ import { ProjectStore } from '../../../../../../mobx/project'
 import { mainStyles } from '../../styles'
 import { observer } from 'mobx-react-lite'
 
+ProjectStore.updateChosenImgs([
+  {
+    id: 1,
+    url: 'https://z3.ax1x.com/2021/08/17/fInXEF.png',
+    name: '图片1'
+  },
+  {
+    id: 2,
+    url: 'https://z3.ax1x.com/2021/08/17/fIn4hj.png',
+    name: '图片2'
+  }
+])
+
 function _Image() {
   return (
     <Box sx={mainStyles.image}>
--- src/pages/ChangeDetection/components/Main/index.tsx
+++ src/pages/ChangeDetection/components/Main/index.tsx
@@ -2,27 +2,16 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import SvgIcon from '../../../../components/SvgIcon'
+import Image from './components/Image'
 import Function from './components/Function'
 import { ProjectStore } from '../../../../mobx/project'
 import { mainStyles } from './styles'
 import { observer } from 'mobx-react-lite'
 
-ProjectStore.updateChosenImgs([
-  {
-    id: 1,
-    url: 'https://z3.ax1x.com/2021/08/17/fInXEF.png',
-    name: '图片1'
-  },
-  {
-    id: 2,
-    url: 'https://z3.ax1x.com/2021/08/17/fIn4hj.png',
-    name: '图片2'
-  }
-])
-
 function _Main() {
   return (
     <Box sx={mainStyles.wrapper}>
+      <Image />
       <Function />
     </Box>
   )
