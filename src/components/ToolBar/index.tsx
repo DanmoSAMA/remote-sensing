3d651c2005093de5e162e601a565416e4ca26b98
DanmoSAMA <772523546@qq.com>
2022-06-09 15:28:11 +0800

    尝试并行上传

--- src/components/ToolBar/index.tsx
+++ src/components/ToolBar/index.tsx
@@ -37,6 +37,7 @@ function _ToolBar() {
     const preloadImages = []
     for (let i = 0; i < ProjectStore.imgs.length; i++) {
       const img = new Image()
+      // 将img从数组中移除，减少内存占用
       img.onload = function () {
         const index = preloadImages.indexOf(this)
         if (index !== 1) {
@@ -49,20 +50,60 @@ function _ToolBar() {
   }
 
   function clickToUploadFile(fileList) {
-    const reqData = new FormData()
+    // 并行发送请求
+    // setIsUploading(true)
+    // const reqData = []
+
+    // for (const key in fileList) {
+    //   const formData = new FormData()
+    //   // 项目id
+    //   formData.append('projectID', id)
+    //   // 上传图片数量
+    //   formData.append('imgNum', '1')
+    //   // 图片
+    //   formData.append('img1', fileList[key])
+    //   // uuid
+    //   formData.append('uuid1', generateUUID())
+    //   // 图片名
+    //   formData.append('name1', fileList[key].name)
+
+    //   reqData.push(formData)
+
+    //   if (parseInt(key) === fileList.length - 1) {
+    //     break
+    //   }
+    // }
+
+    // const promiseArr = []
+    // for (let i = 0; i < reqData.length; i++) {
+    //   const item = reqData[i]
+    //   promiseArr.push(uploadFile(item))
+    // }
+    // console.log(promiseArr)
+
+    // return Promise.all(promiseArr).then((res) => {
+    //   getUpdatedImgs(id).then((res) => {
+    //     const data = res.data
+    //     ProjectStore.updateImgs(data.pictures)
+    //     setIsUploading(false)
+    //   })
+    // })
+
+    // 只发送一个请求
+    const formData = new FormData()
 
     // 项目id
-    reqData.append('projectID', id)
+    formData.append('projectID', id)
     // 上传图片数量
-    reqData.append('imgNum', `${fileList.length}`)
+    formData.append('imgNum', `${fileList.length}`)
 
     for (const key in fileList) {
       // 图片
-      reqData.append(`img${parseInt(key) + 1}`, fileList[key])
+      formData.append(`img${parseInt(key) + 1}`, fileList[key])
       // uuid
-      reqData.append(`uuid${parseInt(key) + 1}`, generateUUID())
+      formData.append(`uuid${parseInt(key) + 1}`, generateUUID())
       // 图片名
-      reqData.append(`name${parseInt(key) + 1}`, fileList[key].name)
+      formData.append(`name${parseInt(key) + 1}`, fileList[key].name)
 
       if (parseInt(key) === fileList.length - 1) {
         break
@@ -71,15 +112,12 @@ function _ToolBar() {
 
     setIsUploading(true)
 
-    uploadFile(reqData).then((res) => {
-      if (res.code === 1000) alert('图片已存在')
-      else {
-        getUpdatedImgs(id).then((res) => {
-          const data = res.data
-          ProjectStore.updateImgs(data.pictures)
-          setIsUploading(false)
-        })
-      }
+    uploadFile(formData).then((res) => {
+      getUpdatedImgs(id).then((res) => {
+        const data = res.data
+        ProjectStore.updateImgs(data.pictures)
+        setIsUploading(false)
+      })
     })
   }
 
