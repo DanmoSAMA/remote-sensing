12f676dc804fdd7dd5f616fcef4187cae9a11da4
cuiyuming <cuiyuming@bytedance.com>
2022-07-19 22:41:23 +0800

    杂项

--- vite.config.ts
+++ vite.config.ts
@@ -9,20 +9,7 @@ export default defineConfig({
   envDir: './src/env',
   resolve: {
     alias: {
-      '@/': path.resolve(__dirname, './src'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/styles': path.resolve(__dirname, './src/styles'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/assets': path.resolve(__dirname, './src/assets'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/routes': path.resolve(__dirname, './src/routes'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/mobx': path.resolve(__dirname, './src/mobx'),
-      '@/network': path.resolve(__dirname, './src/network'),
-      '@/consts': path.resolve(__dirname, './src/constant'),
-      '@/plugins': path.resolve(__dirname, './src/plugins'),
-      '@/env': path.resolve(__dirname, './src/env'),
-      '@/types': path.resolve(__dirname, './src/types'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
